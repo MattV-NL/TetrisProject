d4ea059d7aa33734dd8e515302b148a04ec90049
Kayla Normore <kaylanormore@Kaylas-MacBook-Air.local>
2021-10-18 21:10:57 -0230

    changed style of coding of the javascript and renamed tetrominos to constants

---
+++ scripts/constants.js
@@ -0,0 +1,9 @@
+//Constants
+const grid = document.querySelector('.grid');
+const width = 10;
+const scoreDisplay = document.getElementById('score-display');
+const startButton = document.getElementById('start-button');
+const resetButton = document.getElementById('reset-button');
+
+//Variables
+let squares = Array.from(document.querySelectorAll('.grid div'));
